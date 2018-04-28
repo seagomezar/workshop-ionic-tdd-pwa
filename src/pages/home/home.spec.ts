f2e882eb1f93b81fbb57d25c32a4f862b6e380e1
Sebastian Gomez <seagomezar@gmail.com>
2018-04-27 23:36:15 -0500

    adding the skeleton for tests in componentes, writing the UT for component defined

---
+++ src/pages/home/home.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { HomePage } from "./home";
+import { IonicModule, NavController } from "ionic-angular";
+import { NavMock, HoldingsProviderMock } from "../../../test-config/mocks-ionic";
+import { HoldingsProvider } from "../../providers/holdings/holdings";
+
+describe('Home Page', function() {
+
+    let comp: HomePage;
+    let fixture: ComponentFixture<HomePage>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [HomePage],
+            providers: [
+                {
+                    provide: NavController,
+                    useClass: NavMock
+                },
+                {
+                    provide: HoldingsProvider,
+                    useClass: HoldingsProviderMock
+                }
+            ],
+            imports: [
+                IonicModule.forRoot(HomePage)
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(()=>{
+        fixture = TestBed.createComponent(HomePage);
+        comp = fixture.componentInstance;
+        comp.ionViewDidEnter();
+    });
+
+    afterEach(() => {
+        fixture.destroy();
+    });
+
+    it('El componente home debe crearse', () => {
+        expect(comp).toBeDefined();
+    });
+});
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,4 +13,7 @@ export class HomePage {
 
   }
 
+  ionViewDidEnter(): void {
+  }
+
 }
