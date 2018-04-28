a12e5995c727499871be3d93d4012827595a3505
Sebastian Gomez <seagomezar@gmail.com>
2018-04-27 23:32:33 -0500

    Inyecting holdings provider in home and creating the template

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { HoldingsProvider } from '../../providers/holdings/holdings';
 
 @IonicPage()
 @Component({
@@ -8,7 +9,7 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public holdingsProvider: HoldingsProvider) {
 
   }
 
